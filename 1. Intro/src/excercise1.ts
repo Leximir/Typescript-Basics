5f38c0359982b93051de1135738e75ad6bdece6f
Leximir <aleksa.stankovic@proton.me>
2025-07-18 13:22:06 +0200

    Intro finished

---
+++ 1. Intro/src/excercise1.ts
@@ -0,0 +1,13 @@
+export {};
+
+interface Car{
+    brand: string,
+    model: string,
+    characteristics: string[]
+}
+
+const car: Car = {
+    brand: 'Ford',
+    model: 'Mustang',
+    characteristics: ['5000cc', 'RWD']
+}
