7800f0673b3a99ffb9ca5ec3711a2d4a3d2f38b0
Leximir <aleksa.stankovic@proton.me>
2025-07-31 09:23:23 +0200

    Seventh lecture complete

---
+++ 5. Namespaces/src/index.ts
@@ -0,0 +1,19 @@
+export {};
+
+let name: string = "Lexa";
+
+let programmingLanguages: string[] = ['C', 'C++', 'C#'];
+
+let result: string | number = 1;
+
+interface User{
+    name: string,
+    age: number
+}
+
+const user: User = {
+    name: 'Lexa',
+    age: 24
+}
+
+console.log(result);
